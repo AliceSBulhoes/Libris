c7b2de52608a3cbe9c46fe25029f66b8053c8d08
Alice <alicesb2006@gmail.com>
2025-01-29 15:01:01 -0300

    Adicionando rotas no site

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Erros from './routes/error/Erros.jsx'
+import Home from './routes/Home.jsx'
+import Books from './routes/Books.jsx'
+import Login from './routes/Login.jsx'
+
+const router = createBrowserRouter([{
+  element: <App/>,
+  path: '/',
+  errorElement: <Erros/>,
+  children:[
+    {element: <Home/>, path: '/' },
+    {element: <Books/>, path: '/books'},
+    {element: <Login/>, path: '/login'}
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )

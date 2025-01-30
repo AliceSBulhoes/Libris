5d4636da9631acd57e9286e35eb2872b3d496970
Alice <alicesb2006@gmail.com>
2025-01-30 15:05:09 -0300

    Criando a estrutura do componente Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
-        <h2>Dashboard</h2>
+    <div className='p-8 bg-gray-100 min-h-screen flex-1'>
+        <h2 className='text-2xl font-bold'>Dashboard</h2>
     </div>
   )
 }

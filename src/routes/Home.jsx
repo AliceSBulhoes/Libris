dc77e2a9a8eaa807a67643f1c8c77df29042e76b
Alice <alicesb2006@gmail.com>
2025-01-31 15:31:28 -0300

    Adicionando comentários no Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
+// Importando dependências
 import { Link } from 'react-router-dom'
+// Importando imagem da logo
+import logo from '../assets/logo.svg'
 
 const Home = () => {
 
@@ -9,7 +10,9 @@ const Home = () => {
       <div className="relative isolate h-[90dvh] px-6 pt-14 lg:px-8 flex justify-center items-center flex-col">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center flex flex-col justify-center items-center">
+            {/* Imagem da Logo do site */}
             <img src={logo} alt='logo' className="h-10dvh w-80"/>
+            {/* Mensagem de boas vindas */}
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-paleta-500 sm:text-7xl">
               Bem-Vindo a Libris!
             </h1>
@@ -17,12 +20,14 @@ const Home = () => {
               Uma plataforma desenvolvida para facilitar a busca e a exploração de livros, oferecendo acesso rápido e intuitivo a uma ampla variedade de títulos.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
+              {/* Redireciona para a página principal */}
               <Link to="/books"
               
                 className="rounded-md bg-paleta-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-paleta-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Comece aqui
               </Link>
+              {/* Redireciona para a página sobre nós */}
               <a className="text-sm/6 font-semibold text-gray-900">
                 Sobre Nós <span aria-hidden="true">→</span>
               </a>

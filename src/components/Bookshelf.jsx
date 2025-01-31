156c008ead53d4628014ca11d745e05777758ed3
Alice <alicesb2006@gmail.com>
2025-01-31 16:55:05 -0300

    Criando a requisição a API via barra de pesquisa

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import SearchBar from './SearchBar'
+import { useState } from 'react';
+import { TbBook } from 'react-icons/tb';
+import SearchBar from './SearchBar';
+import request from 'superagent';
 
 const Bookshelf = () => {
-  return (
-    <div className='p-8 bg-paleta-200 min-h-screen flex-1'>
-        <h1 className='text-2xl font-bold'>Bookshelf</h1>
-        <SearchBar/>
-    </div>
-  )
-}
+    const [books, setBooks] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-export default Bookshelf
+    const handleSearch = (e) => {
+        console.log(e.target.value)
+        setSearchField(e.target.value);
+    };
+
+    const searchBook = (e) => {
+        e.preventDefault();
+        request
+            .get(`https://www.googleapis.com/books/v1/volumes?q=${searchField}`)
+            .then((data) => {
+                console.log(data)
+            })
+
+    }
+
+    return (
+        <div className='p-8 bg-paleta-200 min-h-screen flex-1 flex flex-col gap-6'>
+            <div className='flex justify-start items-center gap-2'>
+                <TbBook size={30} color='#402313'/>
+                <h1 className='text-2xl font-bold text-paleta-500 uppercase'>Bookshelf</h1>
+            </div>
+            <SearchBar searchBook={searchBook} handleSearch={handleSearch}/>
+        </div>
+    );
+};
+
+export default Bookshelf;

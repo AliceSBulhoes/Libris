394a3bea43aaf6f26f8b0ce10027c86e555665f2
Alice <alicesb2006@gmail.com>
2025-01-31 16:18:42 -0300

    Alterando estilo de responsividade

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import SearchBar from './SearchBar'
 
 const Bookshelf = () => {
   return (
     <div className='p-8 bg-paleta-200 min-h-screen flex-1'>
         <h1 className='text-2xl font-bold'>Bookshelf</h1>
+        <SearchBar/>
     </div>
   )
 }

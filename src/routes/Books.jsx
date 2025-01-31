5afd9f373e991bbaea8c6f2fc6cab4bf2d0d6863
Alice <alicesb2006@gmail.com>
2025-01-31 15:28:41 -0300

    Adicionando comentários no Books.jsx

--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+// Importando componentes
 import Sidebar from '../components/Sidebar'
 import Dashboard from '../components/Dashboard'
 
 const Books = () => {
   return (
     <div className='flex'>
+      {/* Importando componente do side bar */}
       <Sidebar/>
+      {/* Importando componente do dashboard */}
       <Dashboard/>
     </div>
   )

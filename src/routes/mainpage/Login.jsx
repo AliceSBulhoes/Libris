9027e6db52fdfc051aa85043f1b5edb110949dc4
Alice <alicesb2006@gmail.com>
2025-02-01 16:21:32 -0300

    Alterando mais estilos

--- src/routes/mainpage/Login.jsx
+++ src/routes/mainpage/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='p-8 bg-paleta-200 min-h-screen flex-1 flex flex-col gap-6'>
+    <div className='p-8 bg-paleta-200 min-h-screen flex-1 flex flex-col gap-6 overflow-x-hidden'>
       Login
     </div>
   )

a3e442d23d0e38c3b5db31e3cf172d8c5d3fd0fc
Alice <alicesb2006@gmail.com>
2025-02-01 16:02:26 -0300

    Adicionando a pasta main page no routes e seus componentes

---
+++ src/routes/mainpage/Bookmarks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bookmarks = () => {
+  return (
+    <div className='p-8 bg-paleta-200 min-h-screen flex-1 flex flex-col gap-6'>
+      Bookmarks
+    </div>
+  )
+}
+
+export default Bookmarks
---
+++ src/routes/mainpage/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='p-8 bg-paleta-200 min-h-screen flex-1 flex flex-col gap-6'>
+      Profile
+    </div>
+  )
+}
+
+export default Profile

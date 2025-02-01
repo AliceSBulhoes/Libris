a071695d623a3beeb05b7491b8ccbd2b92262504
Alice <alicesb2006@gmail.com>
2025-02-01 16:04:40 -0300

    Chamando todas as rotas com Outlet

--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -1,6 +1,8 @@
-// Importando componentes
+// Importando dependencias
+import { Outlet } from 'react-router-dom'
+// Importando componente
 import Sidebar from '../components/Sidebar'
-import Bookshelf from '../components/Bookshelf'
+
 
 const MainPage = () => {
   return (
@@ -8,7 +10,7 @@ const MainPage = () => {
       {/* Importando componente do side bar */}
       <Sidebar/>
       {/* Importando componente do dashboard */}
-      <Bookshelf/>
+      <Outlet/>
     </div>
   )
 }

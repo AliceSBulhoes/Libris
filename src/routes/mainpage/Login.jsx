f54a144b1b4db66bd3f10425f7c4ee4dfb6eb1c3
Alice <alicesb2006@gmail.com>
2025-02-02 16:12:17 -0300

    Adicionando comentários no Login

--- src/routes/mainpage/Login.jsx
+++ src/routes/mainpage/Login.jsx
@@ -6,13 +6,17 @@ const Login = () => {
     <div className='p-8 bg-paleta-200 min-h-screen flex-1 flex flex-col gap-6 overflow-x-hidden'>
       <div className="max-w-[480px] mx-auto w-full">
         <div className="flex flex-col items-center mt-[10vh]">
+            {/* Título */}
             <h2 className="mb-5 text-paleta-500 font-mono font-bold text-4xl uppercase">Log In</h2>
+            {/* Botão para logar com o google */}
             <button className="flex items-center mb-2 justify-center transition ease-in-out delay-50 px-3 py-2.5 space-x-2 bg-paleta-400 border border-paleta-500 rounded-md hover:bg-paleta-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-paleta-400 focus:ring-opacity-50">
                 <BsGoogle color='#F2F2F2'/>
                 <span className="text-paleta-100 font-medium">Continue com Google</span>
             </button>
             <span className="mb-2 text-paleta-500 text-2xl font-bold">Ou</span>
+            {/* Form para fazer Login */}
             <form className='flex flex-col gap-5'>
+                {/* Seção Email */}
                 <div>
                   <span className='uppercase font-bold text-paleta-500 text-md'>Email: </span>
                   <input 
@@ -21,6 +25,7 @@ const Login = () => {
                     placeholder="Email"
                     />
                 </div>
+                {/* Seção Senha */}
                 <div>
                   <span className='uppercase font-bold text-paleta-500 text-md'>Senha: </span>
                   <input 
@@ -29,8 +34,10 @@ const Login = () => {
                     placeholder="Senha"
                     />
                 </div>
+                {/* Botão para fazer o Login */}
                 <button className="bg-paleta-500 hover:bg-paleta-400 text-white text-base rounded-lg py-2.5 px-5 transition-colors w-full text-[19px]">Log In</button>
             </form>
+            {/* Redireciona para a página de cadastro */}
             <p className="text-center text-paleta-400 mt-3 text-[14px]">Não tem uma conta?  <a href="/signup" className="text-paleta-500 font-bold uppercase">Cadastre-se</a></p>
         </div>
       </div>

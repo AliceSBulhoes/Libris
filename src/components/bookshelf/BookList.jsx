f94e8654d1c4142ff4d78d03cf5eacd2d033db2d
Alice <alicesb2006@gmail.com>
2025-02-03 16:05:12 -0300

    Adicionando comentários no BookList

--- src/components/bookshelf/BookList.jsx
+++ src/components/bookshelf/BookList.jsx
@@ -1,10 +1,16 @@
+// Importando componente
 import BookCard from './BookCard'
 
 const BookList = ({ books }) => {
   return (
+    // Container para os livros
     <div className='flex flex-wrap gap-10 justify-center'>
         {
+            // Usando map para passar por cada item da
+            // lista book
             books.map((book, i) => {
+                // Para cada item da lista, existe um card
+                // para um livro
                 return <BookCard key={i} book={book}/>
             })
         }
